77f6eab5606b2fc5c05d9552ebe06b100255e0d1
maxim.stetsenko <st_maxwell_@mail.ru>
2016-05-18 16:41:59 +0300

    18-05

---
+++ src/main/java/test1/EchoServer.java
@@ -0,0 +1,97 @@
+package test1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by maxim.stetsenko on 18.05.2016.
+ */
+public class EchoServer extends Thread
+{
+    protected static boolean serverContinue = true;
+    protected Socket clientSocket;
+
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(10008);
+            System.out.println ("Connection Socket Created");
+            try {
+                while (serverContinue)
+                {
+                    System.out.println ("Waiting for Connection");
+                    new EchoServer (serverSocket.accept());
+                }
+            }
+            catch (IOException e)
+            {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not listen on port: 10008.");
+            System.exit(1);
+        }
+        finally
+        {
+            try {
+                serverSocket.close();
+            }
+            catch (IOException e)
+            {
+                System.err.println("Could not close port: 10008.");
+                System.exit(1);
+            }
+        }
+    }
+
+    private EchoServer (Socket clientSoc)
+    {
+        clientSocket = clientSoc;
+        start();
+    }
+
+    public void run()
+    {
+        System.out.println ("New Communication Thread Started");
+
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+                    true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader( clientSocket.getInputStream()));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null)
+            {
+                System.out.println ("Server: " + inputLine);
+
+                out.println(inputLine);
+
+                if (inputLine.equals("Bye."))
+                    break;
+
+                if (inputLine.equals("End Server."))
+                    serverContinue = false;
+            }
+
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Problem with Communication Server");
+            System.exit(1);
+        }
+    }
+}
